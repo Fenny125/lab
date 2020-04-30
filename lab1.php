68ac1803f4dd6da51c4b13af707d694c406fecd3
Fenny125 <64074442+Fenny125@users.noreply.github.com>
2020-04-30 14:56:09 +0300

    Update lab1.php

--- lab1.php
+++ lab1.php
@@ -1,18 +1,26 @@
 
 <?php
-include_once 'user.php';
-$con = new DBConnector;
 
-if (isset ($_POST['btn-save'])){
+include_once 'user.php';
+
+
+if(isset($_POST['btn-save'])){
     $first_name =$_POST ['first_name'];
 	$last_name =$_POST ['last_name'];
     $city =$_POST ['city_name'];
+	$username =$_POST['username'];
+	$password=$_POST['password'];
 
-$user = new User($first_name, $last_name, $city);
+$user = new user($first_name, $last_name, $city, $username, $password);
+if(!$user->validateForm()){
+	$user->createFormErrorSessions();
+	header("Refresh:0");
+	die();
+}
 $res = $user->save();
 
 if ($res){
-echo "Saved!";
+echo "Save operation was successful!";
 }
 else{
 echo "An error occured!";
@@ -23,24 +31,53 @@ echo "An error occured!";
 <!DOCTYPE html>
 <html>
 
-<head> <title> PHP lab 1 </title>
+<head> 
+      <title> PHP lab 1 </title>
+	  <script type ="text/javascript" src="validate.js"></script>
+	  <link rel="stylesheet" type="text/css" href="validate.css">
 
 </head>
 
 <body>
 
 
-<form action="post" method ="POST">
-    <input type="text" name="first_name" placeholder="first_name" required/><br/>
-            <input type="text" name="last_name" placeholder="last_name" required/><br/>
-            <input type="text" name="city_name" placeholder="city" required/><br/>
-            <button type="submit" name="btn-save">Save>SAVE</button>
- 
- 
- 
-
-</form>
-
+<form method="post" name="user_details" onsubmit="return validateForm()" action="<?=$_SERVER['PHP_SELF']?>">
+		<table align="center">
+		    <tr>
+			<td>
+			    <div id ="form-errors">
+				     <?php
+					      session_start();
+						  if(!empty($_SESSION['form_errors'])){
+							  echo" " . $_SESSION['form_errors'];
+							  unset ($_SESSION['form_errors']);
+						  }
+						  ?>
+			    </div>
+				
+			<tr>
+				<td><input type="text" name="first_name" required placeholder="First Name"></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="last_name" placeholder="Last Name"></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="city_name" placeholder="City"></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="username" placeholder="Username"></td>
+			</tr>
+			<tr>
+				<td><input type="password" name="password" placeholder="Password"></td>
+			</tr>
+			<tr>
+				<td><button type="submit" name="btn-save"><strong>SAVE</strong></button></td>
+			</tr>
+			<tr>
+			    <td><a href="login.php">Login</a></td>
+			</tr>
+		</table>		
+	</form>
 
 
 </body>
